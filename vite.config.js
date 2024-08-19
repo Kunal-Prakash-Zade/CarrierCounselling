1eea1220c961498b2f0c416cd6e780655377344c
kunal zade <85743894+Kunal-Prakash-Zade@users.noreply.github.com>
2024-08-19 21:27:52 +0530

    Describe your changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/CarrierCounselling/', // Replace <repository-name> with your actual repo name
+  base: '/prototype/', // Replace <repository-name> with your actual repo name
   plugins: [react()]
 })
